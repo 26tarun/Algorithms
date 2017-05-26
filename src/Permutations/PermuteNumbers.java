5b048b6092a821b6d09002615c12af2cae5bcd22
=26tarun <=tarunjoshi2603@gmail.com>
2017-05-26 00:28:22 -0700

    minor changes

--- src/Permutations/PermuteNumbers.java
+++ src/Permutations/PermuteNumbers.java
@@ -5,45 +5,43 @@ import java.util.HashSet;
 import java.util.List;
 
 public class PermuteNumbers {
-	/*
-	 * Method to invoke recursive method with initialized list and set
-	 * 
-	 */
 	public List<List<Integer>> permuteUnique(int[] nums) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		HashSet<List<Integer>> theSet = new HashSet<List<Integer>>();
-		return recurse(nums, 0, result, theSet);
+		return recurse(nums, 0, result, theSet, true);
+
 	}
 
-	/*
-	 * Recursive method to generate permutations step 1) iterate through this
-	 * array of numbers and store it in a list step 2) start from index 0 and
-	 * compare each element step 3) if different then go ahead and swap them and
-	 * have a unique arrangement of array step 4) move to next index after this
-	 * and recursively call the same method
-	 */
-	public List<List<Integer>> recurse(int[] nums, int s, List<List<Integer>> result, HashSet<List<Integer>> theSet) {
+	public List<List<Integer>> recurse(int[] nums, int s, List<List<Integer>> result, HashSet<List<Integer>> theSet,
+			boolean add) {
 		if (s == nums.length)
 			return result;
 
 		List<Integer> thisList = new ArrayList<Integer>();
-		for (int i = 0; i < nums.length; i++) {
-			thisList.add(nums[i]);
+		// add=true if we want to add this combination
+		// add=false if we just want to explore further combinations since s was
+		// equal to i
+		if (add) {
+			for (int i = 0; i < nums.length; i++) {
+				thisList.add(nums[i]);
+			}
 		}
 		if (thisList.size() > 0 && !theSet.contains(thisList)) {
 			result.add(thisList);
 			theSet.add(thisList);
 		}
+
 		thisList = new ArrayList<Integer>();
 		for (int i = s; i < nums.length; i++) {
 			if (swap(nums, s, i)) {
-				recurse(nums, s + 1, result, theSet);
+				recurse(nums, s + 1, result, theSet, true);
 				swap(nums, s, i);
 			} else {
-				recurse(nums, s + 1, result, theSet);
+				recurse(nums, s + 1, result, theSet, false);
 			}
 		}
-
+		// if(thisList.size()>0 && !theSet.contains(thisList))
+		// result.add(thisList);
 		return result;
 	}
 
